087ff4db08b3d9389a5f7bd57cb0cb15e2281cb3
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2023-02-14 09:51:18 -0300

    feat: fixes and code refactor

--- addons/godot-version-management/godot_version_manager.gd
+++ addons/godot-version-management/godot_version_manager.gd
@@ -2,77 +2,104 @@
 tool
 extends EditorPlugin
 
-const PLUGIN_NAME = "Godot-Version-Manager"
-const DEBUG = true
-# Use same name as https://github.com/godotengine/godot/pull/35555
-const PROJECT_VERSION_SETTING = "application/config/version"
-const PROJECT_BUILD_SETTING = "application/config/build"
-const EXPORT_PRESETS_FILE = "res://export_presets.cfg" 
-var current_version
-var current_build
 
-func _enter_tree():
+# Constants
+const PLUGIN_NAME := "Godot-Version-Manager"
+const DEBUG := true
+# Use same setting as https://github.com/godotengine/godot/pull/35555
+const PROJECT_VERSION_SETTING := "application/config/version"
+const PROJECT_BUILD_SETTING := "application/config/build"
+const EXPORT_PRESETS_FILE := "res://export_presets.cfg"
+
+
+# Variables
+var current_version: String
+var current_build: int
+
+
+# Built-in overrides
+func _enter_tree() -> void:
 	if not ProjectSettings.has_setting(PROJECT_VERSION_SETTING):
 		ProjectSettings.set_setting(PROJECT_VERSION_SETTING, "0.0.1")
+
 	if not ProjectSettings.has_setting(PROJECT_BUILD_SETTING):
 		ProjectSettings.set_setting(PROJECT_BUILD_SETTING, 1)
+
 	current_version = ProjectSettings.get_setting(PROJECT_VERSION_SETTING)
 	current_build = ProjectSettings.get_setting(PROJECT_BUILD_SETTING)
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	# Do not remove the verson config, may conflict with https://github.com/godotengine/godot/pull/35555
 	pass
 
 
-func apply_changes():
+func apply_changes() -> void:
 	_update_export_presets()
 
 
-func save_external_data():
+func save_external_data() -> void:
 	_update_export_presets()
 
 
-func _update_export_presets():
-	# If config version changed, update all exports
-	if ProjectSettings.get_setting(PROJECT_VERSION_SETTING) != current_version:
-		var export_config: ConfigFile = ConfigFile.new()
-		var err = export_config.load(EXPORT_PRESETS_FILE)
-		if err == OK:
-			# Loop limited to 100 exports
-			for i in range(0, 100):
-				var section = "preset." + str(i)
-				if export_config.has_section(section):
-					plugin_log("Update Export " + export_config.get_value(section, "platform"))
-					# Update Android exports configs
-					if export_config.get_value(section, "platform") == "Android":
-						export_config.set_value(section + ".options", 'version/name', ProjectSettings.get_setting(PROJECT_VERSION_SETTING))
-						export_config.set_value(section + ".options", 'version/code', ProjectSettings.get_setting(PROJECT_BUILD_SETTING))
-					if export_config.get_value(section, "platform") == "iOS" or export_config.get_value(section, "platform") == "Mac OSX":
-						export_config.set_value(section + ".options", 'application/short_version', ProjectSettings.get_setting(PROJECT_VERSION_SETTING))
-						export_config.set_value(section + ".options", 'application/version', ProjectSettings.get_setting(PROJECT_BUILD_SETTING))
-					if export_config.get_value(section, "platform") == "UWP":
-						# TODO parsing of version to minor/major
-						pass
-					if export_config.get_value(section, "platform") == "Windows Desktop":
-						export_config.set_value(section + ".options", 'application/file_version', ProjectSettings.get_setting(PROJECT_VERSION_SETTING))
-						export_config.set_value(section + ".options", 'application/product_version', ProjectSettings.get_setting(PROJECT_VERSION_SETTING))
-				else:
-					break
-			err = export_config.save(EXPORT_PRESETS_FILE)
-			ProjectSettings.save()
-			if err == OK:
-				plugin_log("All exports updated")
-			else:
-				plugin_log("Error saving " + EXPORT_PRESETS_FILE + ", exports not updated")
-		else:
-			plugin_log('Error open ' + EXPORT_PRESETS_FILE)
+# Private methods
+func _update_export_presets() -> void:
+	var version_setting: String = ProjectSettings.get_setting(PROJECT_VERSION_SETTING)
+	var build_setting: int = ProjectSettings.get_setting(PROJECT_BUILD_SETTING)
+
+	# If config version not changed, do not update all exports
+	if version_setting == current_version:
+		return
+
+	var export_config := ConfigFile.new()
+	var err := export_config.load(EXPORT_PRESETS_FILE)
+
+	if err != OK:
+		_plugin_log('Error open ' + EXPORT_PRESETS_FILE)
+		return
+
+	# Loop limited to 100 exports
+	for i in range(0, 100):
+		var section := "preset." + str(i)
+
+		if not export_config.has_section(section):
+			break
+
+		_plugin_log("Update Export " + export_config.get_value(section, "platform"))
+
+		# Update Android exports configs
+		if export_config.get_value(section, "platform") == "Android":
+			export_config.set_value(section + ".options", 'version/name', version_setting)
+			export_config.set_value(section + ".options", 'version/code', build_setting)
+
+		# Update Apple exports configs
+		if export_config.get_value(section, "platform") == "iOS" or export_config.get_value(section, "platform") == "Mac OSX":
+			export_config.set_value(section + ".options", 'application/short_version', version_setting)
+			export_config.set_value(section + ".options", 'application/version', build_setting)
+
+		if export_config.get_value(section, "platform") == "UWP":
+			# TODO parsing of version to minor/major
+			pass
+
+		# Update Windows exports configs
+		if export_config.get_value(section, "platform") == "Windows Desktop":
+			var windows_version_setting := "0." + version_setting if version_setting.split(".").size() <= 3 else version_setting
+			export_config.set_value(section + ".options", 'application/file_version', windows_version_setting)
+			export_config.set_value(section + ".options", 'application/product_version', windows_version_setting)
+
+	err = export_config.save(EXPORT_PRESETS_FILE)
+	ProjectSettings.save()
+
+	if err != OK:
+		_plugin_log("Error saving " + EXPORT_PRESETS_FILE + ", exports not updated")
+		return
+
+	_plugin_log("All exports updated")
 
 
-func plugin_log(message):
+func _plugin_log(message: String) -> void:
 	if (DEBUG):
-		var time : Dictionary = OS.get_datetime()
-		var date_string : String = "%02d:%02d" % [time.hour, time.minute]
+		var time := Time.get_datetime_dict_from_system()
+		var date_string := "%02d:%02d" % [time.hour, time.minute]
 		print(date_string, " - ", PLUGIN_NAME, " - ", message)
-	
 
